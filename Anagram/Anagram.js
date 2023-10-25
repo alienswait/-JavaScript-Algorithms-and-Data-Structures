53072c6230d90eb1c23b7188c6c78a2a9fb5a2ce
Mert can Ünek <mrtcnunk@gmail.com>
2023-10-25 16:33:30 +0300

    Update Anagram.js

--- Anagram/Anagram.js
+++ Anagram/Anagram.js
@@ -1,34 +1,29 @@
-function validAnagram(first, second){
-
-    if(first.lenght !== second.lenght){
-        return false;
+function validAnagram(first, second) {
+    if (first.length !== second.length) {
+      return false;
     }
-
+  
     const lookup = {};
-
-    for (let i = 0; i < first.lenght; i++){
-        let letter = first[i];
-        // if letter exist, increment, otherwise set to 1
-        lookup[letter] ? lookup[letter] +1 : lookup[letter] = 1;
-
+  
+    for (let i = 0; i < first.length; i++) {
+      let letter = first[i];
+      // if letter exists, increment, otherwise set to 1
+      lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
     }
-
     console.log(lookup)
-
-    for (let i = 0; i < second.lenght; i++){
-        let letter = second[i];
-        // can't find letter or letter is zero then its not an anagram
-        if(!lookup[letter]){
-            return false;
-        }else{
-            lookup[letter] -= 1;
-        }
+  
+    for (let i = 0; i < second.length; i++) {
+      let letter = second[i];
+      // can't find letter or letter is zero then it's not an anagram
+      if (!lookup[letter]) {
+        return false;
+      } else {
+        lookup[letter] -= 1;
+      }
     }
-
+  
     return true;
-
-
-    
-}
-
-validAnagram('anagram', 'nagaram')
+  }
+  
+  // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+  validAnagram('anagrams', 'nagaramm')
